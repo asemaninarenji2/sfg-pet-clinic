8a15bedb62d7e79be4c0fc27bb1dcbb07f8b2cd3
Moh Mohai <asemaninarenji@gmail.com>
2022-04-03 18:24:11 +1000

    initial project add

---
+++ src/test/java/com/mainapp/mo2/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mainapp.mo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
